c1bb04c5dbdbe83d0a30f6f89306f953006f44f8
Smit703 <smitbagul777@gmail.com>
2022-09-06 08:37:15 +0530

    //send invite SMS

---
+++ Otto.Todo.AuthAzureFunc.API/InviteUserFunc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Otto.Todo.AuthAzureFunc.Core.Interfaces;
+using Otto.Todo.AuthAzureFunc.Models.DTOs;
+using Otto.Todo.AuthAzureFunc.Models.Models;
+
+namespace Otto.Todo.AuthAzureFunc.API
+{
+    public class InviteUserFunc
+    {
+        private readonly IAuthCoreService _authService;
+
+        public InviteUserFunc(IAuthCoreService authService)
+        {
+            _authService = authService;
+        }
+
+        [FunctionName("InviteUserFunc")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "v1/invite/{phone_number}")] HttpRequest req,
+            ILogger log,
+            long phone_number)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            try
+            {
+                _authService.inviteUserAsync(phone_number);
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult(new ErrorDetails()
+                {
+                    StatusCode = 500,
+                    ErrorMessage = ex.Message
+                })
+                {
+                    StatusCode = 500
+                };
+            }
+
+            return new OkObjectResult(new ErrorDetails()
+            {
+                StatusCode = 200
+            })
+            {
+                StatusCode = 200
+            };
+
+        }
+    }
+}
---
+++ Otto.Todo.AuthAzureFunc.Core/Utilities/SMSInviteUtil.cs
@@ -0,0 +1,30 @@
+﻿using Otto.Todo.AuthAzureFunc.Core.Interfaces;
+using Otto.Todo.AuthAzureFunc.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace Otto.Todo.AuthAzureFunc.Core.Utilities
+{
+    public static class SMSInviteUtil
+    {
+        public static void sendSMSToCustomer(long phone)
+        {
+            var accountSid = Environment.GetEnvironmentVariable("ACCOUNT_SID");
+            var authToken = Environment.GetEnvironmentVariable("AUTH_TOKEN");
+            TwilioClient.Init(accountSid, authToken);
+
+            var messageOptions = new CreateMessageOptions(
+                new PhoneNumber(phone.ToString()));
+            messageOptions.MessagingServiceSid = Environment.GetEnvironmentVariable("MESSAGING_SERVICE_SID");
+            messageOptions.Body = "I'm inviting you to use (#name) app ";
+
+            var message = MessageResource.Create(messageOptions);
+            //Console.WriteLine(message.Body);
+            //return message;
+        }
+
+    }
+}
